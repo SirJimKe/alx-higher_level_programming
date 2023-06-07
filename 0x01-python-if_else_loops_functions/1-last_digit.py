b6c0d9fd11a1314f857831e80ea54625e5edde1f
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-07 01:16:40 +0000

    ✨✨ last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = abs(number) % 10
+
+print(num)
+print(f"Last digit of {number} is {num}", end=" ")
+if num > 5:
+    print("and is greater than 5")
+elif num == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,2 +1,2 @@
 * File: 0-positive_or_negative.py - prints whether the number stored in the variable number is positive or negative.
-
+* File: 1-last_digit.py - prints the last digit of the number stored in the variable number
