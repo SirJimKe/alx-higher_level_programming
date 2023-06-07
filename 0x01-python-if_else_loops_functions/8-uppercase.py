49ae0c4183ab139b376e7b91922f4957b38878f8
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-07 22:11:31 +0000

    ✨✨ to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """prints a string in uppercase followed by new line"""
+    result = ""
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print(result)
