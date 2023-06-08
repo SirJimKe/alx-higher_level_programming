93d25ce95d6a4abf09991babce97e1e765a31ffd
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-08 16:26:34 +0000

    ✨✨ enumerate argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""prints the number of and the list of its arguments."""
+if __name__ == "__main__":
+    import sys
+
+    arguments = sys.argv[1:]
+    args_num = len(arguments)
+
+    if args_num == 1:
+        print("1 argument:")
+    elif args_num == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(args_num))
+
+    for i, argument in enumerate(arguments):
+        print("{}: {}".format(i, argument))
