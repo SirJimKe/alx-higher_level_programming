c4a259b6f37b61ba7e9d7f03519fbdb7a73b49e5
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-08 01:29:33 +0000

    ✨✨ byte code

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    """creates a copy of the string, removing the character at the position n"""
+    """removes a character at the position n"""
     string = ""
     for i, char in enumerate(str):
         if i == n:
