3ea723ae9b1d891618a1f707d7dcfbd7854f89e9
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-26 22:44:17 +0000

    ✨✨✨ list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    a, b = my_list_1, my_list_2
+
+    try:
+        for i in range(list_length):
+            try:
+                if not isinstance(a[i], (int, float)) or not isinstance(b[i], (int, float)):
+                    raise TypeError("wrong type")
+                if b[i] == 0:
+                    raise ZeroDivisionError("division by 0")
+                result.append(a[i] / b[i])
+            except IndexError:
+                print("out of range")
+                result.append(0)
+            except (TypeError, ZeroDivisionError) as e:
+                print(e)
+                result.append(0)
+
+    finally:
+        return result
