4bcb133799639ccc5deac7134938ac52d4c9c9e2
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-27 18:10:16 +0000

    ✨✨✨ setter for attributes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" class square with instance attribute"""
+
+
+class Square:
+    """ Class that defines a square
+
+    The Square class provides a basic representation of a square
+
+    Attributes:
+        __size (int): The size of the square
+    Methods:
+        area(): returns the current square area
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of a square
+
+        Returns:
+            int: The area of the square
+        """
+        return self.__size ** 2
