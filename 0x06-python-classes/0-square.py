a95ef5b7f8a88cd73b806fe54158e844244ec60c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-27 16:40:47 +0000

    ✨✨✨ empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """ Empty class that defines a square
+
+    The Square class provides a basic representation of a square
+
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+* File: 0-square.py - is an empty class Square that defines a square
