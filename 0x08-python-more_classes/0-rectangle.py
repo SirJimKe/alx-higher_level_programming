e8916b1ae2d7e9e4992b1a543eb62b2632659121
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-07-03 13:29:06 +0000

    ✨✨✨ empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This is an empty class Rectangle"""
+
+
+class Rectangle:
+    """ Class that defines a rectangle
+
+    The Rectangle class provides basic representation of a rectangle
+
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+* File: 0-rectangle.py - is an empty class Rectangle that defines a rectangle
