12bca6887db162daa9e57a7c3eee363f323d1dd3
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-07-05 20:55:46 +0000

    typographical errors

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width mush be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
