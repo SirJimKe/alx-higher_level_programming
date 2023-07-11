99f5bcf1c65b1239ba8287734ff219d79c38f62d
SirJimKe <jimmymutuku1@gmail.com>
2023-07-11 01:32:45 +0000

    find area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry-related classes.
+
+    Methods:
+        area - finds area
+    """
+    def area(self):
+        """Finds area of a figure"""
+        raise Exception("area() is not implemented")
