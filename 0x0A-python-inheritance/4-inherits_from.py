8c81d01592a61c5e4d2d104ad48e07f81b70283f
SirJimKe <jimmymutuku1@gmail.com>
2023-07-11 01:14:53 +0000

    onl subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Checks if an object is only a subclass of a class"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited
+    (directly or indirectly) from a_class; otherwise False
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
