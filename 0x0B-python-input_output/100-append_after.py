254460340a7678e76648141c5282b28816f978ad
SirJimKe <jimmymutuku1@gmail.com>
2023-07-12 01:54:50 +0000

    output fixed

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,9 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, 'r') as f:
         lines = f.readlines()
+        f.seek(0)
 
     with open(filename, 'w') as f:
         for line in lines:
             f.write(line)
             if search_string in line:
-                f.write(new_string + "\n")
+                f.write(new_string)
