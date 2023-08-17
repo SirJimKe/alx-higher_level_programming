1a29b68b52719a4218417b0d0870a77ddda8091a
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-17 04:45:00 +0300

    select permission only

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Create the database if it doesn't exist
+-- Create the user if it doesn't exist
+-- Grant SELECT privilege to the user on the database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
