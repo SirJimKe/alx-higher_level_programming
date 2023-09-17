42ff5ab81094f55488d67f1d24470758631f9d37
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-09-17 17:04:49 +0300

    SQL injection protection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module lists all states with N as the first letter"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+
+    cur = db.cursor()
+    state_name = sys.argv[4]
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE %s""", (state_name, ))
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
