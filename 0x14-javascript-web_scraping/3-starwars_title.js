40319f00ae414949b5193e9dd60191104dd75328
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-25 03:19:56 +0300

    fetch star wars titles

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log(movieData.title);
+    } catch (parseError) {
+      console.error(parseError.message);
+    }
+  }
+});
