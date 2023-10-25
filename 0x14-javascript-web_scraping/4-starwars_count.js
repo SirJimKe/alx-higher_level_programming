de4ebc9f33aac75f04939d4f25b6385dce1d9162
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-25 03:36:31 +0300

    star wars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      const wedgeAntillesMovies = movieData.results.filter(movie =>
+        movie.characters.includes(
+          'https://swapi-api.alx-tools.com/api/people/18/')
+      );
+      console.log(wedgeAntillesMovies.length);
+    } catch (parseError) {
+      console.error(parseError.message);
+    }
+  }
+});
