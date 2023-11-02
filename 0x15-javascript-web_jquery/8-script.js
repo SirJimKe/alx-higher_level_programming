45fceeb953b91d8b2acae462b45ccb8839bd05d6
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-11-02 04:04:09 +0300

    Say Hello!

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    const movies = data.results;
+    const listItems = movies.map(movie => `<li>${movie.title}</li>`).join('');
+    $('UL#list_movies').html(listItems);
+});
